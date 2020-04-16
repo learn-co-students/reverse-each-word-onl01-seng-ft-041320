20321115cb0b8bec8e25cac4bbedc2b383f39ae1
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-16 16:51:25 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+require 'pry'
+def reverse_each_word(words)
+    array = words.split
+    new_sentence = []
+    array.collect do |word|
+        word.reverse!
+        new_sentence << word
+    end
+    new_sentence.join(" ")
+end
